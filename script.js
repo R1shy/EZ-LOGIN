b16f9f242e0458824396eb45109b33118aba042d
R1shy <kadlecekemma@gmail.com>
2024-09-20 20:46:51 +0000

    styled on them

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let user = document.getElementById("username").value
-let pass = document.getElementById("password").value
+let userr = document.getElementById("username")
+let passs = document.getElementById("password")
 let hide = document.getElementById("hidden")
+
+
 function login() {
-  if (user == "R1shy" || user == "r1shy") {
-    hide.textContent = "button clicked by: dev"
+  let user = userr.value
+  let pass = passs.value
+  if (user == "dev") {
+    window.location.replace("https://56bc8295-eedd-444b-a7ba-f7b2cea5c7d6-00-2uus94n76fhvl.spock.replit.dev/example.html")
   }
-  else {
-    hide.textContent = "button clicked by: " + user
-  }
+  else { alert("normal login!") }
 }
